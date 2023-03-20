973e97d45ddbd858ec6117c111abf927d9109597
Gordon Kamer <gkamer@college.harvard.edu>
2023-03-20 13:01:42 -0400

    coding prompts

--- data_gen.py
+++ data_gen.py
@@ -1,5 +1,7 @@
 import openai
 from secrets import OPENAI_API_KEY, OPENAI_ORG_ID
+import json
+import os
 
 
 def get_gpt_answer_prompt(instruction, context):
@@ -41,11 +43,14 @@ def get_gpt_answer(instruction, context=None):
     return completion
 
 
-def get_more_prompts(example_file):
+def get_more_prompts(prompt_file, example_file):
     with open(example_file, "r") as fhand:
         examples = fhand.read().rstrip().split("\n")
+    with open(prompt_file, "r") as fhand:
+        prompt = fhand.read().rstrip()
+
     listed = "\n".join([f"{i+1}. {examples[i]}" for i in range(len(examples))])
-    prompt = f"Write a list of 20 tasks that a programmer might ask an AI assistant for a quick answer:\n\n{listed}"
+    prompt = f"{prompt}:\n\n{listed}"
     
 
     openai.organization = OPENAI_ORG_ID
@@ -55,7 +60,7 @@ def get_more_prompts(example_file):
         prompt=prompt,
         max_tokens=2048,
         temperature=0.7,
-        presence_penalty=1,
+        presence_penalty=.5,
         frequency_penalty=0,
         best_of=2
     )
@@ -63,9 +68,17 @@ def get_more_prompts(example_file):
     # the zero implies k = 1
     completion = response['choices'][0]['text']
     lines = completion.split("\n")
-    qs = [x[x.index(".") + 2:] for x in lines if "." in x]
+    qs = examples + [x[x.index(".") + 2:] for x in lines if "." in x]
     return qs
 
 if __name__ == '__main__':
-    qs = get_more_prompts("data_gen_prompts/example_coding_prompts.txt")
+    prompts_dir = os.path.join("data_gen_prompts", "coding")
+
+    outfile = os.path.join(prompts_dir, "coding_prompts.json")
+    gen_qs_prompt_path = os.path.join(prompts_dir, "gen_coding_qs_prompt.txt")
+    example_qs_path = os.path.join(prompts_dir, "example_coding_prompts.txt")
+
+    qs = get_more_prompts(gen_qs_prompt_path, example_qs_path)
     print(qs)
+    json.dump(qs, open(outfile, "w"))
+    
