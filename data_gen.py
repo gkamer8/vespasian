5a374edccfd70dc868b2e774db5b4ddf7cfffe8b
Gordon Kamer <gkamer@college.harvard.edu>
2023-03-20 00:47:47 -0400

    simple coding examples

--- data_gen.py
+++ data_gen.py
@@ -29,7 +29,7 @@ def get_gpt_answer(instruction, context=None):
     openai.api_key = OPENAI_API_KEY
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=instruction,
+        prompt=prompt,
         max_tokens=512,
         temperature=0.7,
         presence_penalty=1,
@@ -38,8 +38,34 @@ def get_gpt_answer(instruction, context=None):
 
     # the zero implies k = 1
     completion = response['choices'][0]['text']
-    print(completion)
+    return completion
 
+
+def get_more_prompts(example_file):
+    with open(example_file, "r") as fhand:
+        examples = fhand.read().rstrip().split("\n")
+    listed = "\n".join([f"{i+1}. {examples[i]}" for i in range(len(examples))])
+    prompt = f"Write a list of 20 tasks that a programmer might ask an AI assistant for a quick answer:\n\n{listed}"
+    
+
+    openai.organization = OPENAI_ORG_ID
+    openai.api_key = OPENAI_API_KEY
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=2048,
+        temperature=0.7,
+        presence_penalty=1,
+        frequency_penalty=0,
+        best_of=2
+    )
+
+    # the zero implies k = 1
+    completion = response['choices'][0]['text']
+    lines = completion.split("\n")
+    qs = [x[x.index(".") + 2:] for x in lines if "." in x]
+    return qs
 
 if __name__ == '__main__':
-    get_gpt_answer("Please write javascript code that reverses an array")
+    qs = get_more_prompts("data_gen_prompts/example_coding_prompts.txt")
+    print(qs)
